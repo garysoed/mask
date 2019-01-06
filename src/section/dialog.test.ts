393e1a709cd644b84319d8e410f51e055b69c57c
Gary Soedarsono <garysoed@gmail.com>
2019-01-05 20:58:14 -0800

    updated all tests

--- src/section/dialog.test.ts
+++ src/section/dialog.test.ts
@@ -1,157 +1,168 @@
-// import { assert, setup, should, test } from 'gs-testing/export/main';
-// import { createSpy } from 'gs-testing/export/spy';
-// import { ImmutableMap } from 'gs-tools/export/collect';
-// import { PersonaTester, PersonaTesterFactory } from 'persona/export/testing';
-// import { BehaviorSubject } from 'rxjs';
-// import { take } from 'rxjs/operators';
-// import { _p, _v } from '../app/app';
-// import { textIconButton } from '../component/text-icon-button';
-// import { icon } from '../display/icon';
-// import { iconWithText } from '../display/icon-with-text';
-// import { ActionEvent } from '../event/action-event';
-// import { backdrop } from './backdrop';
-// import { $, dialog } from './dialog';
-// import { $dialogService, $dialogState, DialogService, DialogState } from './dialog-service';
+import { assert, match, setup, should, test } from 'gs-testing/export/main';
+import { createSpy } from 'gs-testing/export/spy';
+import { ImmutableMap, ImmutableSet } from 'gs-tools/export/collect';
+import { PersonaTester, PersonaTesterFactory } from 'persona/export/testing';
+import { BehaviorSubject } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { _p, _v } from '../app/app';
+import { textIconButton } from '../component/text-icon-button';
+import { icon } from '../display/icon';
+import { iconWithText } from '../display/icon-with-text';
+import { backdrop } from './backdrop';
+import { $, dialog } from './dialog';
+import { $dialogService, $dialogState, DialogService, DialogState } from './dialog-service';
 
-// const config = dialog(backdrop(), textIconButton(iconWithText(icon(ImmutableMap.of()))));
-// const testerFactory = new PersonaTesterFactory(_v.builder, _p.builder);
+const config = dialog(backdrop(), textIconButton(iconWithText(icon(ImmutableMap.of()))));
+const testerFactory = new PersonaTesterFactory(_v.builder, _p.builder);
 
-// test('section.Dialog', () => {
-//   let el: HTMLElement;
-//   let tester: PersonaTester;
+test('section.Dialog', () => {
+  let el: HTMLElement;
+  let tester: PersonaTester;
 
-//   setup(() => {
-//     tester = testerFactory.build([config.tag]);
-//     el = tester.createElement('mk-dialog', document.body);
-//     tester.vine.setValue($dialogService, new DialogService());
-//   });
+  setup(() => {
+    tester = testerFactory.build([config.tag]);
+    el = tester.createElement('mk-dialog', document.body);
+    tester.vine.setValue($dialogService, new DialogService());
+  });
 
-//   test('onCancelButtonAction_', () => {
-//     should(`close the dialog correctly`, () => {
-//       const mockOnClose = createSpy('OnClose');
-//       tester.vine.getObservable($dialogService)
-//           .pipe(take(1))
-//           .subscribe(dialogService => {
-//             dialogService.open<number>({
-//               cancelable: true,
-//               elementProvider: () => document.createElement('div'),
-//               onClose: mockOnClose,
-//               title: 'title',
-//             });
-//           });
+  test('onCancelButtonAction_', () => {
+    should(`close the dialog correctly`, () => {
+      const mockOnClose = createSpy('OnClose');
+      tester.vine.getObservable($dialogService)
+          .pipe(take(1))
+          .subscribe(dialogService => {
+            dialogService.open<number>({
+              cancelable: true,
+              elementProvider: () => document.createElement('div'),
+              onClose: mockOnClose,
+              title: 'title',
+            });
+          });
 
-//       const stateSubject = new BehaviorSubject<DialogState|null>(null);
-//       tester.vine.getObservable($dialogState).subscribe(stateSubject);
+      const stateSubject = new BehaviorSubject<DialogState|null>(null);
+      tester.vine.getObservable($dialogState).subscribe(stateSubject);
 
-//       tester.getElement_(el, $.cancelButton.el).click();
+      tester.getElement(el, $.cancelButton)
+          .pipe(take(1))
+          .subscribe(el => el.click());
 
-//       assert(mockOnClose).to.haveBeenCalledWith(true);
+      assert(mockOnClose).to.haveBeenCalledWith(true);
 
-//       // tslint:disable-next-line:no-non-null-assertion
-//       assert(stateSubject.getValue()!.isOpen).to.beFalse();
-//     });
-//   });
+      // tslint:disable-next-line:no-non-null-assertion
+      assert(stateSubject.getValue()!.isOpen).to.beFalse();
+    });
+  });
 
-//   test('onOkButtonAction_', () => {
-//     should(`close the dialog correctly`, () => {
-//       const mockOnClose = createSpy('OnClose');
-//       tester.vine.getObservable($dialogService)
-//           .pipe(take(1))
-//           .subscribe(dialogService => {
-//             dialogService.open<number>({
-//               cancelable: true,
-//               elementProvider: () => document.createElement('div'),
-//               onClose: mockOnClose,
-//               title: 'title',
-//             });
-//           });
+  test('onOkButtonAction_', () => {
+    should(`close the dialog correctly`, () => {
+      const mockOnClose = createSpy('OnClose');
+      tester.vine.getObservable($dialogService)
+          .pipe(take(1))
+          .subscribe(dialogService => {
+            dialogService.open<number>({
+              cancelable: true,
+              elementProvider: () => document.createElement('div'),
+              onClose: mockOnClose,
+              title: 'title',
+            });
+          });
 
-//       const stateSubject = new BehaviorSubject<DialogState|null>(null);
-//       tester.vine.getObservable($dialogState).subscribe(stateSubject);
+      const stateSubject = new BehaviorSubject<DialogState|null>(null);
+      tester.vine.getObservable($dialogState).subscribe(stateSubject);
 
-//       tester.getElement_(el, $.okButton.el).click();
+      tester.getElement(el, $.okButton)
+          .pipe(take(1))
+          .subscribe(el => el.click());
 
-//       assert(mockOnClose).to.haveBeenCalledWith(false);
+      assert(mockOnClose).to.haveBeenCalledWith(false);
 
-//       // tslint:disable-next-line:no-non-null-assertion
-//       assert(stateSubject.getValue()!.isOpen).to.beFalse();
-//     });
-//   });
+      // tslint:disable-next-line:no-non-null-assertion
+      assert(stateSubject.getValue()!.isOpen).to.beFalse();
+    });
+  });
 
-//   test('renderCancelButtonClasses_', () => {
-//     should(`display the cancel button if cancelable`, () => {
-//       const mockOnClose = createSpy('OnClose');
-//       tester.vine.getObservable($dialogService)
-//           .pipe(take(1))
-//           .subscribe(dialogService => {
-//             dialogService.open<number>({
-//               cancelable: true,
-//               elementProvider: () => document.createElement('div'),
-//               onClose: mockOnClose,
-//               title: 'title',
-//             });
-//           });
+  test('renderCancelButtonClasses_', () => {
+    should(`display the cancel button if cancelable`, async () => {
+      const mockOnClose = createSpy('OnClose');
+      tester.vine.getObservable($dialogService)
+          .pipe(take(1))
+          .subscribe(dialogService => {
+            dialogService.open<number>({
+              cancelable: true,
+              elementProvider: () => document.createElement('div'),
+              onClose: mockOnClose,
+              title: 'title',
+            });
+          });
 
-//       assert(tester.getClassList_(el, $.cancelButton.classlist)).to.haveElements(['isVisible']);
-//     });
+      await assert(tester.getClassList(el, $.cancelButton)).to.emitWith(
+          match.anyIterableThat<string, ImmutableSet<string>>().haveElements(['isVisible']),
+      );
+    });
 
-//     should(`not display the cancel button if not cancelable`, () => {
-//       const mockOnClose = createSpy('OnClose');
-//       tester.vine.getObservable($dialogService)
-//           .pipe(take(1))
-//           .subscribe(dialogService => {
-//             dialogService.open<number>({
-//               cancelable: false,
-//               elementProvider: () => document.createElement('div'),
-//               onClose: mockOnClose,
-//               title: 'title',
-//             });
-//           });
+    should(`not display the cancel button if not cancelable`, async () => {
+      const mockOnClose = createSpy('OnClose');
+      tester.vine.getObservable($dialogService)
+          .pipe(take(1))
+          .subscribe(dialogService => {
+            dialogService.open<number>({
+              cancelable: false,
+              elementProvider: () => document.createElement('div'),
+              onClose: mockOnClose,
+              title: 'title',
+            });
+          });
 
-//       assert(tester.getClassList_(el, $.cancelButton.classlist)).to.beEmpty();
-//     });
-//   });
+      await assert(tester.getClassList(el, $.cancelButton)).to.emitWith(
+        match.anyIterableThat<string, ImmutableSet<string>>().beEmpty(),
+      );
+    });
+  });
 
-//   test('renderRootClass_', () => {
-//     should(`render correctly when dialog is displayed`, () => {
-//       tester.vine.getObservable($dialogService)
-//           .pipe(take(1))
-//           .subscribe(dialogService => {
-//             dialogService.open<number>({
-//               cancelable: false,
-//               elementProvider: () => document.createElement('div'),
-//               onClose: () => undefined,
-//               title: 'title',
-//             });
-//           });
+  test('renderRootClass_', () => {
+    should(`render correctly when dialog is displayed`, async () => {
+      tester.vine.getObservable($dialogService)
+          .pipe(take(1))
+          .subscribe(dialogService => {
+            dialogService.open<number>({
+              cancelable: false,
+              elementProvider: () => document.createElement('div'),
+              onClose: () => undefined,
+              title: 'title',
+            });
+          });
 
-//       assert(tester.getClassList_(el, $.root.classlist)).to.haveElements(['isVisible']);
-//     });
+      await assert(tester.getClassList(el, $.root)).to.emitWith(
+        match.anyIterableThat<string, ImmutableSet<string>>().haveElements(['isVisible']),
+      );
+    });
 
-//     should(`render correctly when dialog is hidden`, () => {
-//       assert([...tester.getClassList_(el, $.root.classlist)]).to.haveExactElements([]);
-//     });
-//   });
+    should(`render correctly when dialog is hidden`, async () => {
+      await assert(tester.getClassList(el, $.root)).to.emitWith(
+        match.anyIterableThat<string, ImmutableSet<string>>().beEmpty(),
+      );
+    });
+  });
 
-//   test('renderTitle_', () => {
-//     should(`render the title correctly`, () => {
-//       const title = 'title';
-//       tester.vine.getObservable($dialogService)
-//           .pipe(take(1))
-//           .subscribe(dialogService => {
-//             dialogService.open<number>({
-//               cancelable: false,
-//               elementProvider: () => document.createElement('div'),
-//               onClose: () => undefined,
-//               title,
-//             });
-//           });
+  test('renderTitle_', () => {
+    should(`render the title correctly`, async () => {
+      const title = 'title';
+      tester.vine.getObservable($dialogService)
+          .pipe(take(1))
+          .subscribe(dialogService => {
+            dialogService.open<number>({
+              cancelable: false,
+              elementProvider: () => document.createElement('div'),
+              onClose: () => undefined,
+              title,
+            });
+          });
 
-//       assert(tester.getTextContent_(el, $.title.text)).to.equal(title);
-//     });
+      await assert(tester.getTextContent(el, $.title)).to.emitWith(title);
+    });
 
-//     should(`render empty string when dialog is hidden`, () => {
-//       assert(tester.getTextContent_(el, $.title.text)).to.equal('');
-//     });
-//   });
-// });
+    should(`render empty string when dialog is hidden`, async () => {
+      await assert(tester.getTextContent(el, $.title)).to.emitWith('');
+    });
+  });
+});
--- src/section/drawer.test.ts
+++ src/section/drawer.test.ts
@@ -1,74 +1,73 @@
-// import { assert, setup, should, test } from 'gs-testing/export/main';
-// import { PersonaTester, PersonaTesterFactory } from 'persona/export/testing';
-// import { _p, _v } from '../app/app';
-// import { $, drawer, Mode } from './drawer';
+import { assert, setup, should, test } from 'gs-testing/export/main';
+import { PersonaTester, PersonaTesterFactory } from 'persona/export/testing';
+import { _p, _v } from '../app/app';
+import { $, drawer, Mode } from './drawer';
 
-// const config = drawer();
-// const testerFactory = new PersonaTesterFactory(_v.builder, _p.builder);
+const config = drawer();
+const testerFactory = new PersonaTesterFactory(_v.builder, _p.builder);
 
-// test('section.Drawer', () => {
-//   let el: HTMLElement;
-//   let tester: PersonaTester;
+test('section.Drawer', () => {
+  let el: HTMLElement;
+  let tester: PersonaTester;
 
-//   setup(() => {
-//     tester = testerFactory.build([config.tag]);
-//     el = tester.createElement('mk-drawer', document.body);
-//   });
+  setup(() => {
+    tester = testerFactory.build([config.tag]);
+    el = tester.createElement('mk-drawer', document.body);
+  });
 
-//   test('renderStyleHeight_', () => {
-//     should(`render the max size if horizontal and expanded`, async () => {
-//       const size = '123px';
-//       await tester.setAttribute_(el, $.host.mode, Mode.HORIZONTAL);
-//       await tester.setAttribute_(el, $.host.maxSize, size);
-//       await tester.setAttribute_(el, $.host.expanded, true);
+  test('renderStyleHeight_', () => {
+    should(`render the max size if horizontal and expanded`, async () => {
+      const size = '123px';
+      tester.setAttribute(el, $.host._.mode, Mode.HORIZONTAL).subscribe();
+      tester.setAttribute(el, $.host._.maxSize, size).subscribe();
+      tester.setAttribute(el, $.host._.expanded, true).subscribe();
 
-//       assert(tester.getStyle(el, $.host.style.height)).to.equal(size);
-//     });
+      await assert(tester.getStyle(el, $.host._.styleHeight)).to.emitWith(size);
+    });
 
-//     should(`render the min size if horizontal and collapsed`, async () => {
-//       const size = '123px';
-//       await tester.setAttribute_(el, $.host.mode, Mode.HORIZONTAL);
-//       await tester.setAttribute_(el, $.host.minSize, size);
-//       await tester.setAttribute_(el, $.host.expanded, false);
+    should(`render the min size if horizontal and collapsed`, async () => {
+      const size = '123px';
+      tester.setAttribute(el, $.host._.mode, Mode.HORIZONTAL).subscribe();
+      tester.setAttribute(el, $.host._.minSize, size).subscribe();
+      tester.setAttribute(el, $.host._.expanded, false).subscribe();
 
-//       assert(tester.getStyle(el, $.host.style.height)).to.equal(size);
-//     });
+      await assert(tester.getStyle(el, $.host._.styleHeight)).to.emitWith(size);
+    });
 
-//     should(`render '' if vertical`, async () => {
-//       await tester.setAttribute_(el, $.host.mode, Mode.VERTICAL);
-//       await tester.setAttribute_(el, $.host.minSize, '123px');
-//       await tester.setAttribute_(el, $.host.expanded, true);
+    should(`render '' if vertical`, async () => {
+      tester.setAttribute(el, $.host._.mode, Mode.VERTICAL).subscribe();
+      tester.setAttribute(el, $.host._.minSize, '123px').subscribe();
+      tester.setAttribute(el, $.host._.expanded, true).subscribe();
 
-//       assert(tester.getStyle(el, $.host.style.height)).to.equal('');
-//     });
-//   });
+      await assert(tester.getStyle(el, $.host._.styleHeight)).to.emitWith('');
+    });
+  });
 
-//   test('renderStyleWidth_', () => {
-//     should(`render the max size if vertical and expanded`, async () => {
-//       const size = '123px';
-//       await tester.setAttribute_(el, $.host.mode, Mode.VERTICAL);
-//       await tester.setAttribute_(el, $.host.maxSize, size);
-//       await tester.setAttribute_(el, $.host.expanded, true);
+  test('renderStyleWidth_', () => {
+    should(`render the max size if vertical and expanded`, async () => {
+      const size = '123px';
+      tester.setAttribute(el, $.host._.mode, Mode.VERTICAL).subscribe();
+      tester.setAttribute(el, $.host._.maxSize, size).subscribe();
+      tester.setAttribute(el, $.host._.expanded, true).subscribe();
 
-//       assert(tester.getStyle(el, $.host.style.width)).to.equal(size);
-//     });
+      await assert(tester.getStyle(el, $.host._.styleWidth)).to.emitWith(size);
+    });
 
-//     should(`render the min size if vertical and collapsed`, async () => {
-//       const size = '123px';
-//       await tester.setAttribute_(el, $.host.mode, Mode.VERTICAL);
-//       await tester.setAttribute_(el, $.host.minSize, size);
-//       await tester.setAttribute_(el, $.host.expanded, false);
+    should(`render the min size if vertical and collapsed`, async () => {
+      const size = '123px';
+      tester.setAttribute(el, $.host._.mode, Mode.VERTICAL).subscribe();
+      tester.setAttribute(el, $.host._.minSize, size).subscribe();
+      tester.setAttribute(el, $.host._.expanded, false).subscribe();
 
-//       assert(tester.getStyle(el, $.host.style.width)).to.equal(size);
-//     });
+      await assert(tester.getStyle(el, $.host._.styleWidth)).to.emitWith(size);
+    });
 
-//     should(`render '' if horizontal`, async () => {
-//       const size = '123px';
-//       await tester.setAttribute_(el, $.host.mode, Mode.HORIZONTAL);
-//       await tester.setAttribute_(el, $.host.minSize, '123px');
-//       await tester.setAttribute_(el, $.host.expanded, true);
+    should(`render '' if horizontal`, async () => {
+      tester.setAttribute(el, $.host._.mode, Mode.HORIZONTAL).subscribe();
+      tester.setAttribute(el, $.host._.minSize, '123px').subscribe();
+      tester.setAttribute(el, $.host._.expanded, true).subscribe();
 
-//       assert(tester.getStyle(el, $.host.style.width)).to.equal('');
-//     });
-//   });
-// });
+      await assert(tester.getStyle(el, $.host._.styleWidth)).to.emitWith('');
+    });
+  });
+});
