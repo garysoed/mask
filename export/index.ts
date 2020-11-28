b2fb0fe8fabad8ee7a4c014dd20d849db63413b9
Gary Soedarsono <garysoed@gmail.com>
2020-11-27 17:43:53 -0800

    Exported BaseThemedCtrl

--- export/index.ts
+++ export/index.ts
@@ -1,42 +1,43 @@
 // app
-export { _p, _v, $window } from '../src/app/app';
+export {_p, _v, $window} from '../src/app/app';
 
 // core
-export { registerSvg, $svgService, SvgService } from '../src/core/svg-service';
-export { stateIdParser } from '../src/core/state-id-parser';
-export { SvgConfig } from '../src/core/svg-config';
-export { $stateService } from '../src/core/state-service';
-export { $rootId, $saveConfig, $saveService, SaveService } from '../src/core/save-service';
+export {registerSvg, $svgService, SvgService} from '../src/core/svg-service';
+export {stateIdParser} from '../src/core/state-id-parser';
+export {SvgConfig} from '../src/core/svg-config';
+export {$stateService} from '../src/core/state-service';
+export {$rootId, $saveConfig, $saveService, SaveService} from '../src/core/save-service';
 
 // events
-export { ActionEvent, ACTION_EVENT } from '../src/event/action-event';
-export { ChangeEvent, CHANGE_EVENT } from '../src/event/change-event';
+export {ActionEvent, ACTION_EVENT} from '../src/event/action-event';
+export {ChangeEvent, CHANGE_EVENT} from '../src/event/change-event';
 
 // actions
-export { Button, $button } from '../src/action/button';
+export {Button, $button} from '../src/action/button';
 
 // inputs
-export { Checkbox, $checkbox, CheckedValue } from '../src/action/input/checkbox';
-export { SimpleCheckbox, $simpleCheckbox } from '../src/action/simple/simple-checkbox';
-export { RadioInput, $radioInput } from '../src/action/input/radio-input';
-export { SimpleRadioInput, $simpleRadioInput } from '../src/action/simple/simple-radio-input';
+export {Checkbox, $checkbox, CheckedValue} from '../src/action/input/checkbox';
+export {SimpleCheckbox, $simpleCheckbox} from '../src/action/simple/simple-checkbox';
+export {RadioInput, $radioInput} from '../src/action/input/radio-input';
+export {SimpleRadioInput, $simpleRadioInput} from '../src/action/simple/simple-radio-input';
 
 // display
-export { $annotatedText, AnnotatedText } from '../src/display/annotated-text';
-export { $codeBlock, CodeBlock } from '../src/display/code-block';
-export { $icon, Icon } from '../src/display/icon';
-export { $keyboard, Keyboard, SpecialKeys } from '../src/display/keyboard';
+export {$annotatedText, AnnotatedText} from '../src/display/annotated-text';
+export {$codeBlock, CodeBlock} from '../src/display/code-block';
+export {$icon, Icon} from '../src/display/icon';
+export {$keyboard, Keyboard, SpecialKeys} from '../src/display/keyboard';
 
-export { DrawerLayout, DrawerMode, $drawerLayout } from '../src/layout/drawer-layout';
-export { LayoutOverlay } from '../src/layout/util/layout-overlay';
-export { LineLayout, $lineLayout } from '../src/layout/line-layout';
-export { ListItemLayout, $listItemLayout } from '../src/layout/list-item-layout';
-export { OverlayLayout, $overlayLayout } from '../src/layout/overlay-layout';
-export { Overlay } from '../src/core/overlay';
-export { OverlayService } from '../src/core/overlay-service';
-export { Palette, PALETTE } from '../src/theme/palette';
-export { RootLayout, $rootLayout } from '../src/layout/root-layout';
-export { $theme, start } from '../src/app/app';
-export { Theme } from '../src/theme/theme';
-export { ThemedCustomElementCtrl } from '../src/theme/themed-custom-element-ctrl';
+export {DrawerLayout, DrawerMode, $drawerLayout} from '../src/layout/drawer-layout';
+export {LayoutOverlay} from '../src/layout/util/layout-overlay';
+export {LineLayout, $lineLayout} from '../src/layout/line-layout';
+export {ListItemLayout, $listItemLayout} from '../src/layout/list-item-layout';
+export {OverlayLayout, $overlayLayout} from '../src/layout/overlay-layout';
+export {Overlay} from '../src/core/overlay';
+export {OverlayService} from '../src/core/overlay-service';
+export {Palette, PALETTE} from '../src/theme/palette';
+export {RootLayout, $rootLayout} from '../src/layout/root-layout';
+export {$theme, start} from '../src/app/app';
+export {Theme} from '../src/theme/theme';
+export {ThemedCustomElementCtrl} from '../src/theme/themed-custom-element-ctrl';
+export {BaseThemedCtrl} from '../src/theme/base-themed-ctrl';
 
