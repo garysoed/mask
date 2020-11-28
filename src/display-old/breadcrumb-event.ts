59b3fe6691e5d7dc51a54eb2ebd739b0bb6580f6
Gary Soedarsono <garysoed@gmail.com>
2020-11-28 14:14:57 -0800

    Deleted old components

--- src/display-old/breadcrumb-event.ts
+++
@@ -1,7 +0,0 @@
-export const BREADCRUMB_CLICK_EVENT = 'mk-breadcrumb-click-event';
-
-export class BreadcrumbClickEvent extends Event {
-  constructor(readonly crumbKey: string) {
-    super(BREADCRUMB_CLICK_EVENT);
-  }
-}
