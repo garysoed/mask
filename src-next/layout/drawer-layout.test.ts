0c57a51341df609fa6971d2ded8d0ea3ca477227
Gary Soedarsono <garysoed@gmail.com>
2021-11-27 10:28:09 -0800

    Migrated DrawerLayout

---
+++ src-next/layout/drawer-layout.test.ts
@@ -0,0 +1,68 @@
+import {assert, runEnvironment, should, test} from 'gs-testing';
+import {BrowserSnapshotsEnv} from 'gs-testing/export/browser';
+import {flattenNode, setupTest} from 'persona/export/testing';
+
+import {DrawerMode, DRAWER_LAYOUT} from './drawer-layout';
+import goldens from './goldens/goldens.json';
+
+
+test('@mask/src/layout/drawer-layout', init => {
+  const _ = init(() => {
+    runEnvironment(new BrowserSnapshotsEnv('src-next/layout/goldens', goldens));
+    const tester = setupTest({roots: [DRAWER_LAYOUT]});
+    const element = tester.createElement(DRAWER_LAYOUT);
+    element.textContent = 'content';
+
+    return {element, tester};
+  });
+
+  test('styleHeight$', () => {
+    should('render the max size if horizontal and expanded', () => {
+      _.element.setAttribute('mode', DrawerMode.HORIZONTAL);
+      _.element.setAttribute('max-size', '123px');
+      _.element.setAttribute('expanded', '');
+
+      assert(flattenNode(_.element)).to.matchSnapshot('drawer-layout__horizontal_expanded.html');
+    });
+
+    should('render the min size if horizontal and collapsed', () => {
+      _.element.setAttribute('mode', DrawerMode.HORIZONTAL);
+      _.element.setAttribute('max-size', '123px');
+
+      assert(flattenNode(_.element)).to.matchSnapshot('drawer-layout__horizontal_collapsed.html');
+    });
+
+    should('render \'100%\' if vertical', () => {
+      _.element.setAttribute('mode', DrawerMode.VERTICAL);
+      _.element.setAttribute('max-size', '123px');
+      _.element.setAttribute('expanded', '');
+
+      assert(flattenNode(_.element)).to.matchSnapshot('drawer-layout__vertical_full.html');
+    });
+  });
+
+  test('styleWidth$', () => {
+    should('render the max size if vertical and expanded', () => {
+      _.element.setAttribute('mode', DrawerMode.VERTICAL);
+      _.element.setAttribute('max-size', '123px');
+      _.element.setAttribute('expanded', '');
+
+      assert(flattenNode(_.element)).to.matchSnapshot('drawer-layout__vertical_expanded.html');
+    });
+
+    should('render the min size if vertical and collapsed', () => {
+      _.element.setAttribute('mode', DrawerMode.VERTICAL);
+      _.element.setAttribute('max-size', '123px');
+
+      assert(flattenNode(_.element)).to.matchSnapshot('drawer-layout__vertical_collapsed.html');
+    });
+
+    should('render \'100%\' if horizontal', () => {
+      _.element.setAttribute('mode', DrawerMode.HORIZONTAL);
+      _.element.setAttribute('max-size', '123px');
+      _.element.setAttribute('expanded', '');
+
+      assert(flattenNode(_.element)).to.matchSnapshot('drawer-layout__horizontal_full.html');
+    });
+  });
+});
---
+++ src-next/layout/drawer-layout.ts
@@ -0,0 +1,102 @@
+/**
+ * @webcomponent mk-drawer
+ * Displays a drawer that gets expanded on command.
+ *
+ * @attr {<boolean} expanded True iff the drawer should be expanded.
+ * @attr {<Mode} mode Mode of the drawer. Defaults to vertical.
+ * @attr {<size} minSize Size of the drawer when collapsed.
+ * @attr {<size} maxSize Size of the drawer when expanded.
+ * @slot The content of the drawer.
+ */
+
+import {cache} from 'gs-tools/export/data';
+import {mapNullableTo} from 'gs-tools/export/rxjs';
+import {Context, Ctrl, DIV, iattr, id, iflag, ostyle, registerCustomElement} from 'persona';
+import {combineLatest, Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+
+import {renderTheme} from '../theme/render-theme';
+
+import template from './drawer-layout.html';
+
+
+export enum DrawerMode {
+  HORIZONTAL = 'horizontal',
+  VERTICAL = 'vertical',
+}
+
+export const $drawerLayout = {
+  host: {
+    expanded: iflag('expanded'),
+    maxSize: iattr('max-size'),
+    minSize: iattr('min-size'),
+    mode: iattr('mode'),
+  },
+  shadow: {
+    root: id('root', DIV, {
+      styleHeight: ostyle('height'),
+      styleWidth: ostyle('width'),
+    }),
+  },
+};
+
+export class DrawerLayout implements Ctrl {
+  private readonly maxSize$ = this.$.host.maxSize.pipe(mapNullableTo(''));
+  private readonly minSize$ = this.$.host.minSize.pipe(mapNullableTo('0'));
+
+  constructor(private readonly $: Context<typeof $drawerLayout>) { }
+
+  @cache()
+  get runs(): ReadonlyArray<Observable<unknown>> {
+    return [
+      renderTheme(this.$),
+      this.styleHeight$.pipe(this.$.shadow.root.styleHeight()),
+      this.styleWidth$.pipe(this.$.shadow.root.styleWidth()),
+    ];
+  }
+
+  @cache()
+  private get styleHeight$(): Observable<string> {
+    return combineLatest([
+      this.$.host.expanded,
+      this.maxSize$,
+      this.minSize$,
+      this.$.host.mode,
+    ])
+        .pipe(
+            map(([expanded, maxSize, minSize, mode]) => {
+              if (mode === DrawerMode.VERTICAL) {
+                return '100%';
+              }
+
+              return expanded ? maxSize : minSize;
+            }),
+        );
+  }
+
+  @cache()
+  private get styleWidth$(): Observable<string> {
+    return combineLatest([
+      this.$.host.expanded,
+      this.maxSize$,
+      this.minSize$,
+      this.$.host.mode,
+    ])
+        .pipe(
+            map(([expanded, maxSize, minSize, mode]) => {
+              if (mode === DrawerMode.HORIZONTAL) {
+                return '100%';
+              }
+
+              return expanded ? maxSize : minSize;
+            }),
+        );
+  }
+}
+
+export const DRAWER_LAYOUT = registerCustomElement({
+  ctrl: DrawerLayout,
+  spec: $drawerLayout,
+  tag: 'mk-drawer-layout',
+  template,
+});
