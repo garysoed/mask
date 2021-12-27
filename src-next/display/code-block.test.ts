c3ce9c15ff126986f29bbfea9274d1b2ba2775b5
Gary Soedarsono <garysoed@gmail.com>
2021-12-26 16:18:31 -0800

    Added code block

---
+++ src-next/display/code-block.test.ts
@@ -0,0 +1,34 @@
+import {assert, runEnvironment, should, test} from 'gs-testing';
+import {BrowserSnapshotsEnv} from 'gs-testing/export/browser';
+import {triggerFakeMutation} from 'persona/src-next/testing/fake-mutation-observer';
+
+import {setupThemedTest} from '../testing/setup-themed-test';
+
+import {CODE_BLOCK} from './code-block';
+import goldens from './goldens/goldens.json';
+
+
+test('@mask/src/display/code-block', init => {
+  const _ = init(() => {
+    runEnvironment(new BrowserSnapshotsEnv('src-next/display/goldens', goldens));
+    const tester = setupThemedTest({roots: [CODE_BLOCK]});
+    (window as any).hljs = {
+      highlightBlock: () => undefined,
+    };
+    return {tester};
+  });
+
+  test('render', () => {
+    should('render the content correctly', () => {
+      const element = _.tester.createElement(CODE_BLOCK);
+      element.textContent = `
+      import {something} from './something.ts';
+
+      something.callFn(123);
+      `;
+      triggerFakeMutation(element, {});
+
+      assert(element).to.matchSnapshot('code-block.html');
+    });
+  });
+});
---
+++ src-next/display/code-block.ts
@@ -0,0 +1,55 @@
+import {cache} from 'gs-tools/export/data';
+import {Context, Ctrl, id, itarget, itext, otext, PRE, registerCustomElement} from 'persona';
+import {combineLatest, Observable} from 'rxjs';
+import {tap} from 'rxjs/operators';
+
+import {renderTheme} from '../theme/render-theme';
+
+import template from './code-block.html';
+
+
+interface HighlightJs {
+  highlightBlock(node: Node): void;
+}
+
+declare const hljs: HighlightJs;
+
+export const $codeBlock = {
+  host: {
+    text: itext(),
+  },
+  shadow: {
+    root: id('root', PRE, {
+      target: itarget(),
+      text: otext(),
+    }),
+  },
+};
+
+class CodeBlock implements Ctrl {
+  constructor(private readonly $: Context<typeof $codeBlock>) { }
+
+  @cache()
+  get runs(): ReadonlyArray<Observable<unknown>> {
+    return [
+      renderTheme(this.$),
+      this.$.host.text.pipe(this.$.shadow.root.text()),
+      combineLatest([
+        this.$.host.text,
+        this.$.shadow.root.target,
+      ])
+          .pipe(
+              tap(([, rootTarget]) => {
+                hljs.highlightBlock(rootTarget);
+              }),
+          ),
+    ];
+  }
+}
+
+export const CODE_BLOCK = registerCustomElement({
+  ctrl: CodeBlock,
+  spec: $codeBlock,
+  tag: 'mk-code-block',
+  template,
+});
