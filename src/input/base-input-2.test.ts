b29ba2a8f209d65609206f47fbbf68505b0c4c6c
Gary Soedarsono <garysoed@gmail.com>
2022-09-25 11:41:46 -0700

    Initial check in of base-input-2

---
+++ src/input/base-input-2.test.ts
@@ -0,0 +1,115 @@
+import {assert, runEnvironment, setup, should, test} from 'gs-testing';
+import {BrowserSnapshotsEnv} from 'gs-testing/export/browser';
+import {cache} from 'gs-tools/export/data';
+import {mapNullableTo} from 'gs-tools/export/rxjs';
+import {Context, DIV, iattr, oattr, oflag, query, registerCustomElement} from 'persona';
+import {ElementHarness, getHarness} from 'persona/export/testing';
+import {BehaviorSubject, Observable, OperatorFunction} from 'rxjs';
+import {map} from 'rxjs/operators';
+
+import {$baseRootOutputs} from '../action/base-action';
+import {ActionEvent} from '../event/action-event';
+import {setupThemedTest} from '../testing/setup-themed-test';
+
+import {BaseInput, create$baseInput} from './base-input-2';
+import goldens from './goldens/goldens.json';
+
+
+const $test = {
+  host: {
+    ...create$baseInput<string, string>('').host,
+  },
+  shadow: {
+    div: query('#div', DIV, {
+      ...$baseRootOutputs,
+      disabled: oflag('disabled'),
+      label: oattr('label'),
+      attrIn: iattr('value'),
+      attrOut: oattr('value'),
+    }),
+  },
+};
+
+class TestInput extends BaseInput<string, string> {
+  constructor(private readonly $: Context<typeof $test>) {
+    super(
+        $,
+        $.shadow.div.disabled,
+        $.shadow.div,
+    );
+  }
+
+  get onAction$(): Observable<ActionEvent<string>> {
+    return this.domValue$.pipe(map(value => new ActionEvent(value)));
+  }
+
+  @cache()
+  protected get domValue$(): Observable<string> {
+    return this.$.shadow.div.attrIn.pipe(mapNullableTo(''));
+  }
+
+  protected updateDomValue(): OperatorFunction<string, unknown> {
+    return this.$.shadow.div.attrOut();
+  }
+}
+
+const TEST = registerCustomElement({
+  ctrl: TestInput,
+  spec: $test,
+  tag: 'mk-test-base-input',
+  template: '<div id="div"></div>',
+});
+
+
+test('@mask/src/input/base-input', () => {
+  const _ = setup(() => {
+    runEnvironment(new BrowserSnapshotsEnv('src/input/goldens', goldens));
+    const tester = setupThemedTest({roots: [TEST]});
+    return {tester};
+  });
+
+  test('bindFromHost', () => {
+    should('set the init value', () => {
+      const value$ = new BehaviorSubject('a');
+      const element = _.tester.bootstrapElement(TEST);
+      element.value = value$;
+
+      assert(element).to.matchSnapshot('base-input__init_from_host.html');
+    });
+
+    should('reflect the value from the host', () => {
+      const value$ = new BehaviorSubject('a');
+      const element = _.tester.bootstrapElement(TEST);
+      element.value = value$;
+
+      value$.next('b');
+
+      assert(element).to.matchSnapshot('base-input__from_host.html');
+    });
+  });
+
+  test('bindFromDom', () => {
+    should('skip the init value', () => {
+      const value$ = new BehaviorSubject('a');
+      const element = _.tester.bootstrapElement(TEST);
+      getHarness(element, '#div', ElementHarness).target.setAttribute('value', 'init');
+
+      element.value = value$;
+
+      assert(value$).to.emitWith('a');
+    });
+
+    should('reflect the value from the dom', () => {
+      const value$ = new BehaviorSubject('a');
+      const element = _.tester.bootstrapElement(TEST);
+      getHarness(element, '#div', ElementHarness).target.setAttribute('value', 'init');
+
+      element.value = value$;
+
+      const newValue = 'newValue';
+      getHarness(element, '#div', ElementHarness).target.setAttribute('value', newValue);
+
+      assert(value$).to.emitWith(newValue);
+    });
+  });
+});
---
+++ src/input/base-input-2.ts
@@ -0,0 +1,63 @@
+import {forwardTo} from 'gs-tools/export/rxjs';
+import {instanceofType} from 'gs-types';
+import {Bindings, Context, Ctrl, ivalue} from 'persona';
+import {IValue} from 'persona/export/internal';
+import {BehaviorSubject, merge, Observable, OperatorFunction, Subject} from 'rxjs';
+import {distinctUntilChanged, skip, switchMap} from 'rxjs/operators';
+
+import {$baseRootOutputs, BaseAction, BaseActionSpecType, create$baseAction} from '../action/base-action';
+
+
+export interface BaseInputSpecType<Type, Action> extends BaseActionSpecType<Action> {
+  host: BaseActionSpecType<Action>['host'] & {
+    readonly value: IValue<Subject<Type>, 'value'>;
+  }
+}
+
+export function create$baseInput<Action, Type = never>(defaultValue: Type): BaseInputSpecType<Type, Action> {
+  return {
+    host: {
+      ...create$baseAction<Action>().host,
+      value: ivalue('value', instanceofType<Subject<Type>>(Subject), new BehaviorSubject(defaultValue)),
+    },
+  };
+}
+
+
+export abstract class BaseInput<T, A> extends BaseAction<A> implements Ctrl {
+  constructor(
+      protected readonly inputContext: Context<BaseInputSpecType<T, A>>,
+      renderDisabled: () => OperatorFunction<boolean, unknown>,
+      rootBindings: Bindings<typeof $baseRootOutputs, any>,
+  ) {
+    super(inputContext, renderDisabled, rootBindings);
+  }
+
+  get runs(): ReadonlyArray<Observable<unknown>> {
+    return [
+      ...super.runs,
+      this.inputContext.host.value.pipe(
+          switchMap(subject => {
+            const fromHostToDom$ = subject.pipe(
+                distinctUntilChanged(),
+                this.updateDomValue(),
+            );
+
+            const fromDomToHost$ = this.domValue$.pipe(
+                // Skips the initial emission.
+                skip(1),
+                distinctUntilChanged(),
+                forwardTo(subject),
+            );
+
+            // ORDERING MATTERS!
+            return merge(fromDomToHost$, fromHostToDom$);
+          }),
+      ),
+    ];
+  }
+
+  protected abstract get domValue$(): Observable<T>;
+
+  protected abstract updateDomValue(): OperatorFunction<T, unknown>;
+}
